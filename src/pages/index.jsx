97da464bd3b7f5cd234b56ed0c1faaece52c94cd
yanina <112989135+YANINAMARQUEZ@users.noreply.github.com>
2025-09-19 18:06:41 -0300

    Actualizar index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,8 +10,8 @@ export default function Home() {
       </Head>
       <Header />
       <main className="px-6 py-12">
-        <h1 className="text-3xl font-bold">Bienvenidos a Marquezlab</h1>
-        <p className="mt-4 text-lg">Soluciones digitales con excelencia visual y legal.</p>
+        <h1 className="text-3xl font-bold">Bienvenidos a Márquezlab-webpage</h1>
+        <p className="mt-4 text-lg">Soluciones digitales, servicios de software personaluzado con excelencia visual y legal.</p>
       </main>
       <Footer />
     </>
