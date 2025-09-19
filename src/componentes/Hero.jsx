5561a4d1f76fc94c6f5367ac2c571dbc0358f7e8
yanina <112989135+YANINAMARQUEZ@users.noreply.github.com>
2025-09-19 18:12:38 -0300

    Actualizar Hero.jsx

--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -4,8 +4,8 @@ import './Hero.css';
 export default function Hero() {
   return (
     <section id="inicio" className="hero">
-      <h1>Estrategia digital, identidad institucional y legalidad en una sola solución.</h1>
-      <p>Desarrollo web, branding visual y asesoría legal para marcas que quieren diferenciarse.</p>
+      <h1>Estrategia digital, identidad institucional y , acompañamiento en una en todo tu proyecto.</h1>
+      <p>Desarrollo web, branding visual y asesoría  para marcas que quieren diferenciarse.</p>
       <a href="#contacto" className="hero-btn">Agendar asesoría</a>
     </section>
   );
